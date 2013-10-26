5dd7b01da050a12f54c06945d9cafc08f647be67
Antonio <antonio.hdez93@gmail.com>
2013-10-25 20:28:10 -0500

    Función recursiva
    Fución recursiva para copiar los valores (no funcional al 100%).

--- ProyectoParcial/main.cpp
+++ ProyectoParcial/main.cpp
@@ -6,10 +6,96 @@
 
 using namespace std;
 
-int NumEquipos;
+int N;
 vector<string> equipos;
 vector<int> peso;
-int *tabla;
+int **matriz;
+
+void imprimeMatriz(){
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            cout<<matriz[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int generaPartidos(int x1, int y1, int sX, int sY){
+    if(sX < 1) return 0;
+    else{
+    for(int i = y1; i < sY/2; i++){
+        matriz[x1+sX/2][y1+sY/2+i] = matriz[x1][y1+i];
+        cout<<x1+sX/2<<" - "<<y1+sY/2+i<<endl;
+    }
+        int contY = 0;
+    
+        for(int i = sY/2; i <sY; i++){
+            matriz[x1+sX/2][y1+contY] = matriz[x1][y1+sY/2+ contY];
+            contY++;
+    }
+     
+        
+        /*
+    for(int i = sY/2; i < sY ;i++){
+        matriz[x1+sX/2][posy++] = matriz[x1][y1+i];
+        
+    }
+         */
+        
+        
+    generaPartidos(x1, y1, sX/2, sY/2);
+    generaPartidos(sX/2, y1, sX/2, sY/2);
+    generaPartidos(x1, sY/2, sX/2, sY/2);
+    generaPartidos(sX/2, sY/2, sX/2, sY/2);
+    }
+    
+    return 0;
+}
+
+void generaPartidos(){
+    if(N % 2 == 0){
+            generaPartidos(0, 0, N, N);
+    }else{
+        generaPartidos(0, 0, N+1, N);
+    }
+    
+}
+
+void crearMatriz(){
+    if(N % 2 == 0){
+        matriz = new int *[N];
+        for(int i = 0 ; i < N; i++){
+            matriz[i] = new int[N];
+        }
+        for(int i = 0 ; i < N; i++){
+            for(int j = 0; j < N; j++){
+                if(i == 0){
+                    matriz[i][j] = j+1;
+                }else{
+                    matriz[i][j] = 0;
+                }
+                //cout << matriz[i][j];
+            }
+            //cout<<endl;
+        }
+    }else{
+        matriz = new int *[N+1];
+        for(int i = 0 ; i < N+1; i++){
+            matriz[i] = new int[N];
+        }
+        for(int i = 0 ; i < N+1; i++){
+            for(int j = 0; j < N; j++){
+                if(i == 0){
+                    matriz[i][j] = j+1;
+                }else{
+                    matriz[i][j] = -1;
+                }
+                //cout << matriz[i][j];
+            }
+            //cout<<endl;
+        }
+    }
+}
 
 void leerArchivo(string ruta){
     string delimiter = " ";
@@ -19,10 +105,7 @@ void leerArchivo(string ruta){
     if (myfile.is_open())
     {
         getline(myfile,line);
-        NumEquipos = stoi(line);
-        
-        
-        
+        N = stoi(line);
         while (getline(myfile,line))
         {
             pos = 0;
@@ -45,15 +128,17 @@ void leerArchivo(string ruta){
 
 
 
-int main () {
+int main(){
     
     leerArchivo("/Users/Antonio/Desktop/Algoritmos/ProyectoParcial/Equipos.txt");
+    crearMatriz();
+    generaPartidos();
+    imprimeMatriz();
+    cout<<"Número de equipos: "<<N<<endl;
     
-    cout<<"Número de equipos: "<<NumEquipos<<endl;
-    
-    for(int i =  0; i < NumEquipos; i++){
+    for(int i =  0; i < N; i++){
         cout<<equipos.at(i)<<" "<<peso.at(i)<<endl;
     }
-    
+    cout<<sizeof(matriz)/sizeof(*matriz);
     return 0;
 }
